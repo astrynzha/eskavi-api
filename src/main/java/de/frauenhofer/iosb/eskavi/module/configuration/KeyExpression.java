7c94f496e5ef8ba7164a9eaab1821ba57bda3925
David Kaufmann <david.kaufmann@student.kit.edu>
2021-01-26 18:26:57 +0100

    add all methods to class KeyExpression

--- src/main/java/de/frauenhofer/iosb/eskavi/module/configuration/KeyExpression.java
+++ src/main/java/de/frauenhofer/iosb/eskavi/module/configuration/KeyExpression.java
@@ -1,4 +1,51 @@
 package de.frauenhofer.iosb.eskavi.module.configuration;
 
+/**
+ * Represents the invariant parts of the KeyExpression of a Configuration. Is a simple storage class.
+ */
 public class KeyExpression {
+    private String expressionStart;
+    private String expressionEnd;
+
+    /**
+     * Constructs a new KeyExpression
+     * @param expressionStart
+     * @param expressionEnd
+     */
+    public KeyExpression(String expressionStart, String expressionEnd) {
+        setExpressionStart(expressionStart);
+        setExpressionEnd(expressionEnd);
+    }
+
+    /**
+     * Returns the part of the KeyExpression to be added in front of the value
+     * @return expressionStart
+     */
+    public String getExpressionStart() {
+        return expressionStart;
+    }
+
+    /**
+     * Changes the beginning of the KeyExpression
+     * @param expressionStart String to start the expression
+     */
+    public void setExpressionStart(String expressionStart) {
+        this.expressionStart = expressionStart;
+    }
+
+    /**
+     * Returns the part of the KeyExpression to be added after the value
+     * @return expressionEnd
+     */
+    public String getExpressionEnd() {
+        return expressionEnd;
+    }
+
+    /**
+     * Changes the beginning of the KeyExpression
+     * @param expressionEnd String to start the expression
+     */
+    public void setExpressionEnd(String expressionEnd) {
+        this.expressionEnd = expressionEnd;
+    }
 }
