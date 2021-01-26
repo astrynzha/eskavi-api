f04ac0dff6f27482a9c47d2e172f27b5e22010c4
Niv Adam <nivadam@web.de>
2021-01-26 16:52:27 +0100

    Spring initializer

---
+++ src/main/java/de/frauenhofer/iosb/eskavi/EskaviApplication.java
@@ -0,0 +1,13 @@
+package de.frauenhofer.iosb.eskavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskaviApplication.class, args);
+	}
+
+}
