5d62d8d61706a4c51ea456f927fac19c5c9375a0
David Kaufmann <david.kaufmann@student.kit.edu>
2021-01-26 18:19:32 +0100

    add all methodes inside class Configuration

--- src/main/java/de/frauenhofer/iosb/eskavi/module/configuration/Configuration.java
+++ src/main/java/de/frauenhofer/iosb/eskavi/module/configuration/Configuration.java
@@ -1,5 +1,7 @@
 package de.frauenhofer.iosb.eskavi.module.configuration;
 
+import java.util.Collection;
+
 public abstract class Configuration {
     private String name;
     private boolean allowMultiple;
@@ -11,7 +13,48 @@ public abstract class Configuration {
         this.keyExpression = expression;
     }
 
+    public abstract String resolveKeyExpression();
+    public abstract boolean checkCompatible();
+    @Override
+    public abstract Configuration clone();
+
     public void addChild(Configuration child) {
         //does nothing should not be invoked onto this type
+    }
+
+    public void removeChild(String name) {
+        //does nothing should not be invoked onto this type
+    }
+    /*
+    public Collection<ImmutableModuleImp> getDependentModuleImps() {
+        return null;
+    }*/
+
+    public ImmutableModuleImp getModuleImp() {
+        return null;
+    }
+
+    public KeyExpression getKeyExpression() {
+        return keyExpression;
+    }
+
+    public void setKeyExpression(KeyExpression expression) {
+        this.keyExpression = expression;
+    }
+
+    public boolean allowsMultiple() {
+        return allowMultiple;
+    }
+
+    public void setAllowMultiple(boolean allowMultiple) {
+        this.allowMultiple = allowMultiple;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
