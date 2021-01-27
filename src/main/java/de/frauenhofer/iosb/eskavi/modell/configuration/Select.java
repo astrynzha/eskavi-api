476ca9d13babd30508c6b83053fd240567ad1b99
David Kaufmann <david.kaufmann@student.kit.edu>
2021-01-27 11:46:36 +0100

    add classes Select and Switch

---
+++ src/main/java/de/frauenhofer/iosb/eskavi/modell/configuration/Select.java
@@ -0,0 +1,41 @@
+package de.frauenhofer.iosb.eskavi.modell.configuration;
+
+import java.util.Map;
+
+public class Select extends SingleValueField {
+
+    private Map<String, String> content;
+
+    public Select(String name, boolean allowMultiple, KeyExpression expression, Map<String, String> content) {
+        super(name, allowMultiple, expression);
+        this.content = content;
+    }
+
+    public Map<String, String> getContent() {
+        return content;
+    }
+
+    public void setContent(Map<String, String> content) {
+        this.content = content;
+    }
+
+    @Override
+    public void setValue(String value) {
+        if (content.containsKey(value)) {
+            super.setValue(content.get(value));
+        } else {
+            throw new IllegalArgumentException("value must be a possible value from content");
+        }
+    }
+
+    @Override
+    public String resolveKeyExpression() {
+        return getKeyExpression().getExpressionStart() + getValue() + getKeyExpression().getExpressionEnd();
+    }
+
+    @Override
+    public Select clone() {
+        KeyExpression copy = new KeyExpression(getKeyExpression().getExpressionStart(), getKeyExpression().getExpressionEnd());
+        return new Select(getName(), allowsMultiple(), copy, getContent());
+    }
+}
---
+++ src/main/java/de/frauenhofer/iosb/eskavi/modell/configuration/Switch.java
@@ -0,0 +1,25 @@
+package de.frauenhofer.iosb.eskavi.modell.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Switch extends Select {
+    private static String TRUE = "trueValue";
+    private static String FALSE = "falseValue";
+
+    public Switch(String name, boolean allowMultiple, KeyExpression expression, String trueValue, String falseValue) {
+        super(name, allowMultiple, expression, null);
+        Map<String, String> content = new HashMap<>();
+        content.put(TRUE, trueValue);
+        content.put(FALSE, falseValue);
+        this.setContent(content);
+        //setting the default value
+        this.setValue(FALSE);
+    }
+
+    @Override
+    public Switch clone() {
+        KeyExpression copy = new KeyExpression(getKeyExpression().getExpressionStart(), getKeyExpression().getExpressionEnd());
+        return new Switch(getName(), allowsMultiple(), copy, getContent().get(TRUE), getContent().get(FALSE));
+    }
+}
