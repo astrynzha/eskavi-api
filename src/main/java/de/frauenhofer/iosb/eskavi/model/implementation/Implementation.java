81d42fedf2295fc868924f28e1c557378c0af19d
Niv Adam <nivadam@web.de>
2021-01-28 10:42:26 +0100

    Implement mock subscribe and unsubscribe methods

--- src/main/java/de/frauenhofer/iosb/eskavi/model/implementation/Implementation.java
+++ src/main/java/de/frauenhofer/iosb/eskavi/model/implementation/Implementation.java
@@ -1,8 +1,17 @@
 package de.frauenhofer.iosb.eskavi.model.implementation;
 
+import de.frauenhofer.iosb.eskavi.model.user.User;
+
 /**
  * @author Niv Adam
  * @version 1.0.0
  */
 public abstract class Implementation implements ImmutableImplementation {
+    public void subscribe(User user) {
+        return;
+    }
+
+    public void unsubscribe(User user) {
+        return;
+    }
 }
