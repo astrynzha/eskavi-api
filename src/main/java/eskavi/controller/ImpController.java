8567a3341ca67ff6d2595d2c4e35bf8e3239f3f4
maxgeorg99 <maxi.georg.mg@gmail.com>
2021-01-30 18:20:46 +0100

    Add api documentation and fix typo

--- src/main/java/eskavi/controller/ImpController.java
+++ src/main/java/eskavi/controller/ImpController.java
@@ -1,36 +1,96 @@
 package eskavi.controller;
 
 import eskavi.model.implementation.ImmutableImplementation;
+import eskavi.model.implementation.ImpType;
 import eskavi.model.user.ImmutableUser;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("imp")
 public class ImpController {
+
+    /**
+     * @api{get}/imp/user Get Implementations by user
+     * @apiName GetImplementationByUser
+     * @apiGroup Implementation
+     * @apiVersion 0.0.1
+     * @apiHeader {String} Authorization Authorization header using the Bearer schema: Bearer token
+     * @apiError {String} message Errormessage
+     * @apiErrorExample {json} Error-Response:
+     * HTTP/1.1 404 Not Found
+     * {
+     * "error": "UserNotFound"
+     * }
+     */
     @GetMapping("/user/{userId:[0-9]+}")
-    public Collection<ImmutableUser> getAllByUser(@PathVariable long userId) {
+    public Collection<ImmutableImplementation> getAllByUser(@PathVariable long userId) {
         return null;
     }
 
+    /**
+     * @api{get}/imp/:id Get Implementation
+     * @apiName GetImplementation
+     * @apiGroup Implementation
+     * @apiVersion 0.0.1
+     * @apiHeader {String} Authorization Authorization header using the Bearer schema: Bearer token
+     * @apiParam {Number} id Implementation unique ID
+     * @apiSuccess {Implementation} implementation Implementation object
+     * @apiError {String} message Errormessage
+     * @apiErrorExample {json} Error-Response:
+     * HTTP/1.1 404 Not Found
+     * {
+     * "error": "UserNotFound"
+     * }
+     */
     @GetMapping("/{id:[0-9]+}")
     public ImmutableImplementation get(@PathVariable("id") long impId) {
         return null;
     }
 
-    // TODO Wait for ImpType
-    // public ImmutableImplementation getDefaultImpCreate(ImpType type){}
+    @GetMapping("/default")
+    public ImmutableImplementation getDefaultImpCreate(@PathParam("type") ImpType type) {
+        return null;
+    }
 
     @GetMapping("/template/{id:[0-9]+}")
     public ImmutableImplementation getTemplateImpCreate(@PathVariable("id") long impId) {
         return null;
     }
 
+    /**
+     * @api{post}/imp Post Implementation
+     * @apiName PostImplementation
+     * @apiGroup Implementation
+     * @apiVersion 0.0.1
+     * @apiHeader {String} Authorization Authorization header using the Bearer schema: Bearer token
+     * @apiSuccess {Number} id Implementation unique ID
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 201 Created
+     * {
+     * "id":"1"
+     * }
+     * @apiError {String} message Errormessage
+     * @apiErrorExample {json} Error-Response:
+     * HTTP/1.1 403 Forbidden
+     * {
+     * "error": "Access denied for non publishing user"
+     * }
+     */
     @PostMapping
     public void add(ImmutableImplementation mi) {
     }
 
+    /**
+     * @api{put}/imp Put Implementation
+     * @apiName PostImplementation
+     * @apiGroup Implementation
+     * @apiVersion 0.0.1
+     * @apiHeader {String} Authorization Authorization header using the Bearer schema: Bearer token
+     * @apiError {String} message Errormessage
+     */
     @PutMapping
     public void put(ImmutableImplementation request) {
     }
