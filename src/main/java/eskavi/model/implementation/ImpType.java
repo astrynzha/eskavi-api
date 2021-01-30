4df7794390164ad98b9ccb4c9026a7cfefa5d74a
David Kaufmann <david.kaufmann@student.kit.edu>
2021-01-30 15:06:40 +0100

    change ImpType so it is able to check whether a given instance is of the right type

--- src/main/java/eskavi/model/implementation/ImpType.java
+++ src/main/java/eskavi/model/implementation/ImpType.java
@@ -1,12 +1,24 @@
 package eskavi.model.implementation;
 
+import eskavi.model.implementation.moduleimp.*;
+
 public enum ImpType {
-    ASSET_CONNECTION,
-    DESERIALIZER,
-    DISPATCHER,
-    ENDPOINT,
-    HANDLER,
-    INTERACTION_STARTER,
-    PERSISTENCE_MANAGER,
-    SERIALIZER;
+    ASSET_CONNECTION(AssetConnection.class),
+    DESERIALIZER(Deserializer.class),
+    DISPATCHER(Dispatcher.class),
+    ENDPOINT(Endpoint.class),
+    HANDLER(Handler.class),
+    INTERACTION_STARTER(InteractionStarter.class),
+    PERSISTENCE_MANAGER(PersistenceManager.class),
+    SERIALIZER(Serializer.class);
+
+    private Class matchingClass;
+
+    private ImpType(Class moduleImp) {
+        this.matchingClass = moduleImp;
+    }
+
+    public boolean matches(ImmutableModuleImp input) {
+        return matchingClass.isInstance(input);
+    }
 }
