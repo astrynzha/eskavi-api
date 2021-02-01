8fb479ec4dcb91cd32f247f21a258fc8153e48c7
maxgeorg99 <maxi.georg.mg@gmail.com>
2021-02-01 22:04:02 +0100

    update JavaClassGeneratorTest

--- src/test/java/eskavi/util/JavaClassGeneratorTest.java
+++ src/test/java/eskavi/util/JavaClassGeneratorTest.java
@@ -5,6 +5,12 @@ import com.google.googlejavaformat.java.FormatterException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JavaClassGeneratorTest {
@@ -14,30 +20,35 @@ public class JavaClassGeneratorTest {
 
     @BeforeEach
     void before() {
-        _formattedCode = """
-                public class JavaClassGenerator {
-                    public static File generateClassFile(String code) {
-                        try {
-                            PrintWriter out = new PrintWriter("aas.java");
-                            out.println(format(code));
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        } catch (FormatterException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }""";
-        _rawCode = "public class JavaClassGenerator {public static File generateClassFile(String code) {try {PrintWriter out = new PrintWriter(\"aas.java\");out.println(format(code));} catch (FileNotFoundException e) {e.printStackTrace();} catch (FormatterException e) {e.printStackTrace();}return null;}";
+        _formattedCode = "class HelloWorld {\n" +
+                "  public static void main(String[] args) {\n" +
+                "    System.out.println(\"Hello, World!\");\n" +
+                "  }\n" +
+                "}\n";
+        _rawCode = "class HelloWorld {public static void main(String[] args){System.out.println(\"Hello, World!\");}}";
     }
 
     @Test
     void generateClassFile() {
-
+        File generatedFile = JavaClassGenerator.generateClassFile(_rawCode);
+        File expectedFile = new File("src/test/resources/HelloWorldTestClass.java");
+        assertTrue(isFileEqual(generatedFile, expectedFile));
     }
 
     @Test
     void formatCode() throws FormatterException {
         String afterFormatting = new Formatter().formatSource(_rawCode);
-        assertTrue(afterFormatting.equals(_formattedCode));
+        assertEquals(_formattedCode, afterFormatting);
+    }
+
+    private boolean isFileEqual(File f1, File f2) {
+        try {
+            byte[] first = Files.readAllBytes(f1.toPath());
+            byte[] second = Files.readAllBytes(f2.toPath());
+            return Arrays.equals(first, second);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
