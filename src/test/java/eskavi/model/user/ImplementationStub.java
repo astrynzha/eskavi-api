65a826a028024f2a1f0c5d5cde9305082ce620ce
Andrii Strynzha <a.strynzha@gmail.com>
2021-02-01 16:50:36 +0200

    Fixed User tests

--- src/test/java/eskavi/model/user/ImplementationStub.java
+++ src/test/java/eskavi/model/user/ImplementationStub.java
@@ -1,11 +1,12 @@
 package eskavi.model.user;
 
 import eskavi.model.implementation.Implementation;
+import eskavi.model.implementation.ImplementationScope;
 import eskavi.model.implementation.Scope;
 
 public class ImplementationStub extends Implementation {
 
-    protected ImplementationStub(long implementationId, User author, String name, Scope scope) {
-        super(implementationId, author, name, scope);
+    protected ImplementationStub(long implementationId, User author, String name, ImplementationScope impScope) {
+        super(implementationId, author, name, impScope);
     }
 }
