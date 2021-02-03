1f5c6e4f351259c9a7620e1c6543cfe2ca0d19b7
David Kaufmann <david.kaufmann@student.kit.edu>
2021-02-03 16:12:32 +0100

    add FileField class and defaultpath property

---
+++ src/main/java/eskavi/model/configuration/FileField.java
@@ -0,0 +1,35 @@
+package eskavi.model.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class FileField extends SingleValueField {
+    @Value("${file.defaultpath}")
+    private static String defaultPath;
+
+    public FileField(String name, boolean allowMultiple, KeyExpression expression) {
+        super(name, allowMultiple, expression);
+    }
+
+    @Override
+    public String resolveKeyExpression() {
+        if (getValue() != null) {
+            return getKeyExpression().getExpressionStart() + FileField.defaultPath + getValue() + getKeyExpression().getExpressionEnd();
+        } else {
+            throw new IllegalStateException("A value has to be assigned to Configuration: " + getName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FileField{" +
+                "name='" + getName() + '\'' +
+                ", allowMultiple=" + allowsMultiple() +
+                ", keyExpression=" + getKeyExpression().toString() +
+                ", filename='" + getValue() + "'}";
+    }
+
+    @Override
+    public Configuration clone() {
+        return null;
+    }
+}
