70e2c93602ca2022cc6a2a50f4dffa6c8d420004
Niv Adam <nivadam@web.de>
2021-02-08 14:31:23 +0100

    Add javadoc

--- src/main/java/eskavi/service/aasconfigurationservice/ModuleInstanceFactory.java
+++ src/main/java/eskavi/service/aasconfigurationservice/ModuleInstanceFactory.java
@@ -3,11 +3,22 @@ package eskavi.service.aasconfigurationservice;
 import eskavi.model.implementation.ModuleImp;
 import eskavi.model.implementation.ModuleInstance;
 
+/**
+ * This is a utility class offering a factory method to create {@link ModuleInstance} based on a passed {@link ModuleImp}.
+ *
+ * @author Niv Adam
+ */
 public final class ModuleInstanceFactory{
     private ModuleInstanceFactory() {
         throw new AssertionError("Instantiating utility class");
     }
 
+    /**
+     * Factory method for {@link ModuleInstance}.
+     *
+     * @param mi the {@link ModuleImp} the instance should be based on
+     * @return the instance
+     */
     public static ModuleInstance build(ModuleImp mi) {
         return new ModuleInstance(mi, mi.getConfiguration().clone());
     }
