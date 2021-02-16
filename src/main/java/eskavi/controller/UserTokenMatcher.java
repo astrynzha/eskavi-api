4a5d289885baa9f4cfeeb72541930b1b76a1fac2
maxgeorg99 <maxi.georg.mg@gmail.com>
2021-02-16 19:02:39 +0100

    fix UserTokenMatcher

--- src/main/java/eskavi/controller/UserTokenMatcher.java
+++ src/main/java/eskavi/controller/UserTokenMatcher.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import eskavi.model.user.ImmutableUser;
 import eskavi.repository.UserRepository;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
@@ -27,16 +28,15 @@ public class UserTokenMatcher {
         JWTVerifier verifier = JWT.require(algorithm)
                 .build(); //Reusable verifier instance
         DecodedJWT jwt = verifier.verify(token);
-        String userId = jwt.getHeaderClaim("email").asString();
+        String userId = jwt.getClaim("email").asString();
         return userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
     }
 
     public String generateToken(String userId) {
         Algorithm algorithm = Algorithm.HMAC256("secret");
-        String token = JWT.create()
+        return JWT.create()
                 .withClaim("email", userId)
-                .withExpiresAt(new Date(new Date().getTime() + 2 * 3600 * 1000))
+                .withExpiresAt(DateUtils.addHours(new Date(), 1))
                 .sign(algorithm);
-        return token;
     }
 }
