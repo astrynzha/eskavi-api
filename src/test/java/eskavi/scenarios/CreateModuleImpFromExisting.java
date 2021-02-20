38dedd0c18f577c01c81fd07741489318fece07c
maxgeorg99 <maxi.georg.mg@gmail.com>
2021-02-20 23:44:13 +0100

    Add 2 more empty integration test classes

---
+++ src/test/java/eskavi/scenarios/CreateModuleImpFromExisting.java
@@ -0,0 +1,17 @@
+package eskavi.scenarios;
+
+
+import eskavi.EskaviApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(properties = {"spring.jpa.hibernate.ddl-auto=create-drop"}, classes = EskaviApplication.class)
+@DirtiesContext
+@AutoConfigureMockMvc
+public class CreateModuleImpFromExisting {
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+}
