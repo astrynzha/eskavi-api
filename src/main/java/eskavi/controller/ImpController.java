56b15e53f9d895545a8075f2f016b8b693e490aa
maxgeorg99 <maxi.georg.mg@gmail.com>
2021-03-14 12:18:01 +0100

    Adjust api doc

--- src/main/java/eskavi/controller/ImpController.java
+++ src/main/java/eskavi/controller/ImpController.java
@@ -702,7 +702,7 @@ public class ImpController {
      * @apiGroup Implementation
      * @apiVersion 0.0.1
      * @apiHeader {String} Authorization Authorization header using the Bearer schema: Bearer token
-     * @apiParam (Request body) {Number} impId Implementation unique ID
+     * @apiParam (Request Parameter) {Number} id Implementation unique ID
      * @apiError {String} message Errormessage
      * @apiErrorExample {json} Error-Response:
      * HTTP/1.1 404 Not Found
